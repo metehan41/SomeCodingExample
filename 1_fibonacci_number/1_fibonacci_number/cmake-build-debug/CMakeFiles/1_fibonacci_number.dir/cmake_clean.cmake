078e533c002a06ed9116dc50b61a750d0deda654
metehan41 <53449821+metehan41@users.noreply.github.com>
2023-02-22 22:22:35 +0300

    Add files via upload

---
+++ 1_fibonacci_number/1_fibonacci_number/cmake-build-debug/CMakeFiles/1_fibonacci_number.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_fibonacci_number.exe"
+  "1_fibonacci_number.exe.manifest"
+  "1_fibonacci_number.lib"
+  "1_fibonacci_number.pdb"
+  "CMakeFiles/1_fibonacci_number.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_fibonacci_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

a6285bb24749d6c3d1113e8b6afce67c937e176a
metehan41 <53449821+metehan41@users.noreply.github.com>
2023-02-22 22:24:09 +0300

    Add files via upload

---
+++ 2_last_digit_of_fibonacci_number/2_last_digit_of_fibonacci_number/cmake-build-debug/CMakeFiles/2_last_digit_of_fibonacci_number.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_last_digit_of_fibonacci_number.exe"
+  "2_last_digit_of_fibonacci_number.exe.manifest"
+  "2_last_digit_of_fibonacci_number.lib"
+  "2_last_digit_of_fibonacci_number.pdb"
+  "CMakeFiles/2_last_digit_of_fibonacci_number.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_last_digit_of_fibonacci_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3_greatest_common_divisor/cmake-build-debug/CMakeFiles/3_greatest_common_divisor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_greatest_common_divisor.exe"
+  "3_greatest_common_divisor.exe.manifest"
+  "3_greatest_common_divisor.lib"
+  "3_greatest_common_divisor.pdb"
+  "CMakeFiles/3_greatest_common_divisor.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_greatest_common_divisor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

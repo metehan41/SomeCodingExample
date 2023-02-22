080027476e8d92680a09098847ee1456a16c2180
metehan41 <53449821+metehan41@users.noreply.github.com>
2023-02-22 22:25:47 +0300

    Add files via upload

---
+++ 5_1_money_change_again/cmake-build-debug/CMakeFiles/5_1_money_change_again.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_1_money_change_again.exe"
+  "5_1_money_change_again.exe.manifest"
+  "5_1_money_change_again.lib"
+  "5_1_money_change_again.pdb"
+  "CMakeFiles/5_1_money_change_again.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_1_money_change_again.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 5_2_primitive_calculator/cmake-build-debug/CMakeFiles/5_2_primitive_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5_2_primitive_calculator.exe"
+  "5_2_primitive_calculator.exe.manifest"
+  "5_2_primitive_calculator.lib"
+  "5_2_primitive_calculator.pdb"
+  "CMakeFiles/5_2_primitive_calculator.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_2_primitive_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
